b8a0d2c01a80dc420bedaf1b0b981cf5c546c920
Tanya Hodges <tanyac52003@gmail.com>
2020-06-15 15:39:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,11 @@
 require 'pry'
 
 class Song
+  extend Memorable
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-extend Memorable::ClassMethods
 
   def initialize
     @@songs << self
